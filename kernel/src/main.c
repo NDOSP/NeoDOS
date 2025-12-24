935a1be7d89f12349dd0f3ceabfa74232b104b58
Iskra-YT <iskra42@proton.me>
2025-12-24 15:05:20 +0100

    feat: make build-in font more readable

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -7,12 +7,12 @@ __attribute__((section(".bootinfo"))) BootInfo bInfo;
 
 void kmain() {
     initVideo(&bInfo);
-    drawString("ABCDEFGHIJKL", 0, 0, white);
-    drawString("MNOPRSTUWXYZ", 0, bInfo.font->fontHeight * 2, white);
-    drawString("abcdefghijkl", 0, bInfo.font->fontHeight * 4, white);
-    drawString("mnoprstuwxyz", 0, bInfo.font->fontHeight * 6, white);
-    drawString("1234567890", 0, bInfo.font->fontHeight * 8, white);
-
+    drawString(" !\"#$%&'()*+,-./", 0, 0, white);
+    drawString("0123456789:;<=>?", 0, bInfo.font->fontHeight * bInfo.fontScale * 1, white);
+    drawString("@ABCDEFGHIJKLMNO", 0, bInfo.font->fontHeight * bInfo.fontScale * 2, white);
+    drawString("PQRSTUVWXYZ[\\]^_", 0, bInfo.font->fontHeight * bInfo.fontScale * 3, white);
+    drawString("`abcdefghijklmno", 0, bInfo.font->fontHeight * bInfo.fontScale* 4, white);
+    drawString("pqrstuvwxyz{|}~ ", 0, bInfo.font->fontHeight * bInfo.fontScale* 5, white);
     idtInit();
     asm volatile("ud2");
     asm volatile("hlt");
